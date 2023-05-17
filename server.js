2c65887d56068510a9f7f6122d789728e1b29ce6
Aaron <aaturner1995@gmail.com>
2023-05-17 12:13:34 -0300

    added model and controller layout

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
-const db = require('./config/connection')
+const db = require('./config/connection');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API server is running on port ${PORT}`)
     })
-})
+});
